dcb3b365534ae89f965edb4e942f84e957202c21
Yago <yagolobo.dev@gmail.com>
2024-03-11 17:38:54 -0300

    Adding chat room & username login

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'secretpassword'
 socketio = SocketIO(app)
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/chat')
+def chat():
+    username = request.args.get('username')
+    return render_template('chat.html', username=username)
 
 @socketio.on('message')
-def handle_message(msg):
-    emit('message', msg, broadcast=True)
-
+def handle_message(data):
+    username = data['username']
+    message = data['message']
+    emit('message', {'username': username, 'message': message}, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
+
+
